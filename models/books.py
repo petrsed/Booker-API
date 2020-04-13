9953a8cff19120e69735f22c384f14615c8f602f
Petr Sedelnikov <45389741+PetrSed@users.noreply.github.com>
2020-04-13 22:53:19 +0700

    add image_id and description im book model

--- models/books.py
+++ models/books.py
@@ -7,7 +7,9 @@ class Books(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
     genre_id = sqlalchemy.Column(sqlalchemy.String, index=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     author_id = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     barcode = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    image_id = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     quantity = sqlalchemy.Column(sqlalchemy.String, nullable=True)
